1d8abda8266e1c08d7081af77506c9b276879595
chhinhua <chhinhua@gmail.com>
2023-06-16 19:21:38 +0700

    create movie review

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,82 @@
+import { useEffect, useRef, useState } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+
+const Reviews = ({ getMovieData, movie }) => {
+  const revText = useRef();
+  const [reviews, setReviews] = useState([]);
+  let params = useParams();
+  const movieId = params.movieId;
+
+  useEffect(() => {
+    getMovieData(movieId);
+  }, []);
+
+  const addReview = async (review) => {
+    try {
+      const response = await api.post(`/api/v1/reviews`, {
+        reviewBody: review,
+        imdbId: movieId,
+      });
+
+      setReviews([...reviews, { body: review }]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h3>Reviews</h3>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col>
+          <img src={movie?.poster} alt="" />
+        </Col>
+        <Col>
+          <Row>
+            <Col>
+              <ReviewForm
+                handleSubmit={addReview}
+                revText={revText}
+                labelText="Write a Review?"
+                setReviews={setReviews}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr />
+            </Col>
+          </Row>
+          {reviews?.map((r, index) => {
+            return (
+              <div key={index}>
+                <Row>
+                  <Col>{r.body}</Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <hr />
+                  </Col>
+                </Row>
+              </div>
+            );
+          })}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Reviews;
